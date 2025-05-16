ac11bbb3124c8929da682e0d02cd802184f63a77
sarra <sarahbeji147@gmail.com>
2025-05-16 02:05:09 +0100

    Initial commit: Projet Spring Boot

---
+++ springboot-tpese/src/main/java/com/sarah/springboot_tpese/SpringbootTpeseApplication.java
@@ -0,0 +1,13 @@
+package com.sarah.springboot_tpese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTpeseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTpeseApplication.class, args);
+	}
+
+}
